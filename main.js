5150c7ebd69426ef73829827bc77c6e20e8b6367
AZ_218 <ratmir.miro@gmail.com>
2021-08-25 17:19:34 +0300

    formulaz

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ document.getElementsByTagName('main')[0].innerHTML =
     .replaceAll('AZsSPC', '<span title="" translate="no">AZsSPC</span>')
     .replace('#projects', '<a href="https://AZsSPC.github.io/projects">projects</a>')
 ;
+console.log("1")
--- projects/FormulaZ/main.js
+++ projects/FormulaZ/main.js
@@ -1,17 +1,17 @@
-
 let f_d = document.getElementById('f_d');
 let f_r = document.getElementById('f_r');
 let f_b = document.getElementById('f_b');
 
-function find(charclude){
+console.log("2")
+
+function find(included_char){
     let arr = (f_d.textContent || f_d.innerText).split(/\n+/);
     f_r.innerHTML = '';
-    for(let i = 0; i < arr.length; i++) if(arr[i].includes(charclude)){
+    for(let i = 0; i < arr.length; i++) if(arr[i].includes(included_char)){
         let innerMJ = arr[i].split(/==+/, 2);
         let m_c = document.createElement('span');
         m_c.className = 'm_c';
-        m_c.id = 'm_c-' + i;
-        m_c.setAttribute('onclick', 'smaz(' + i + ')');
+        m_c.setAttribute('onclick', 'switchViewMode(this)');
         let m_h = document.createElement('span');
         m_h.className = 'm_h';
         m_h.innerHTML = '$$' + innerMJ[0] + '$$';
@@ -26,25 +26,24 @@ function find(charclude){
     f_r.hidden = false;
 }
 
-function smaz(mi){
-    let data = document.getElementById('m_c-' + mi);
-    data.className = (data.className == 'm_c' ?'m_cb' :'m_c');
+function switchViewMode(el){
+    el.className = (el.className === 'm_c' ?'m_cb' :'m_c');
 }
 
-function reload_data(){
+function reload_data(url = 'https://azsspc.github.io/projects/FormulaZ/physic.faz'){
     f_r.hidden = true;
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://azsspc.github.io/projects/FormulaZ/physic.faz', true);
+    xhr.open('GET', url, true);
     xhr.send();
     xhr.onreadystatechange = function (){
-        if(xhr.readyState != 4) return;
+        if(xhr.readyState !== 4) return;
         let ret_data = (xhr.responseText).split(/\n*azdatasplit\n*/);
         f_d.innerHTML = ret_data[0];
-        let butts = ret_data[1].split(/\n+/);
+        let buttons = ret_data[1].split(/\n+/);
         f_b.innerHTML = '';
-        for(let i = 0; i < butts.length; i++){
-            if(butts[i].length < 3) continue;
-            let bd = butts[i].split(/==+/);
+        for(let i = 0; i < buttons.length; i++){
+            if(buttons[i].length < 3) continue;
+            let bd = buttons[i].split(/==+/);
             let button = document.createElement('button');
             button.innerHTML = bd[0];
             button.setAttribute('onclick', 'find(\'' + bd[1] + '\')');
@@ -54,4 +53,4 @@ function reload_data(){
     }
 }
 
-reload_data();
+reload_data('https://azsspc.github.io/projects/FormulaZ/physic.faz');
