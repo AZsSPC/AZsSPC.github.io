95de3723a6601656d7e436766b442bf6060d3f40
AZ_218 <ratmir.miro@gmail.com>
2021-09-10 08:14:34 +0300

    fixed!

--- projects/History Line/drawer.js
+++ projects/History Line/drawer.js
@@ -1,6 +1,18 @@
+let ir_w = document.getElementById('ir_w');
+let ir_h = document.getElementById('ir_h');
 let canvas = document.getElementById('sphere');
+let simg = document.getElementById('simg');
+let sc = document.getElementById('sc').getContext('2d');
 let ctx = canvas.getContext('2d');
-let simg = document.getElementById('simg').getAttribute('src');
+let pw = 0, ph = 0;
+ir_w.setAttribute('oninput', 'spt(0)');
+ir_h.setAttribute('oninput', 'spt(1)');
+let points = [{'x': 0, 'y': 125, 'z': 0}]
+let p_m;
+simg.setAttribute('crossOrigin', '');
+sc.drawImage(simg, 0, 0);
+
+draw();
 
 function draw(){
 
@@ -16,8 +28,6 @@ function draw(){
      console.log('asdasd')*/
 }
 
-let points = [{'x': 0, 'y': 250, 'z': 0}]
-
 function rotate(point, rW, rH){
     // rH -= 90;
     rW = rW * Math.PI / 180;
@@ -29,21 +39,44 @@ function rotate(point, rW, rH){
     let ca = Math.cos(rH);
     let sb = Math.sin(rW);
     let cb = Math.cos(rW);
-    let new_point = {
+    return {
         'x': (x * ca + y * sa) * cb - z * sb,
         'y': y * ca - x * sa,
         'z': (x * ca + y * sa) * sb + z * cb
     };
-    let W = (Math.abs(new_point.z + 100) / 10).toFixed(0) / 30;
-    ctx.fillStyle = 'rgba(250, 250, 250, 1)';
-    ctx.fillRect(new_point.x + 300, new_point.y + 300, 2, 2);
-    return new_point;
 }
 
-draw()
-for(let i = 0; i < 180; i++){
-    points[0] = rotate(points[0], 0, 1)
-    for(let j = 0; j < 360; j++)
-        points[0] = rotate(points[0], 1, 0)
+function get_simg_pix(x, y, z){
+    if(x < 0) x += 144;
+    x = x % 144;
+    if(y < 0) y += 72;
+    y = y % 72;
+    let data = sc.getImageData(x, y, 1, 1).data;
+    let f = (z / 2 + 50) / 125;
+    return {'r': data[0] * f, 'g': data[1] * f, 'b': data[2] * f};
 }
+
+//points[0] = rotate(points[0], 0, 0);
+
+function spt(k){
+    if(k === 0) pw = ir_w.value * 1; else ph = ir_h.value * 1;
+    d();
+}
+
+function d(){
+    for(let hd = 0; hd < 72; hd++) for(let wd = 0; wd < 72; wd++){
+        p_m = rotate(points[0], wd * 2.5, hd * 2.5);
+        let c = get_simg_pix(144 - wd + pw, 72 - hd + ph, p_m.z);
+        ctx.fillStyle = 'rgba(' + c.r + ', ' + c.g + ', ' + c.b + ', 1)';
+        ctx.fillRect(p_m.x + 300 - 3, p_m.y + 300 - 3, 6, 6);
+    }
+}
+
+d();
 //setTimeout(sayHi, 1000, "Привет", "Джон");
+/*
+ setTimeout(function run(){
+ d();
+ pw += 5;
+ setTimeout(run, 100);
+ }, 100);*/
--- projects/History Line/main.js
+++ projects/History Line/main.js
@@ -1,70 +1,3 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
-
-function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
-
-    const fov = 75;
-    const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
-
-    const scene = new THREE.Scene();
-
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    const cubes = [];  // just an array we can use to rotate the cubes
-    const loader = new THREE.TextureLoader();
-    loader.load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg', (texture) => {
-        const material = new THREE.MeshBasicMaterial({
-            map: texture,
-        });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-        cubes.push(cube);  // add to our list of cubes to rotate
-    });
-
-    function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-            renderer.setSize(width, height, false);
-        }
-        return needResize;
-    }
-
-    function render(time) {
-        time *= 0.001;
-
-        if (resizeRendererToDisplaySize(renderer)) {
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();
-        }
-
-        cubes.forEach((cube, ndx) => {
-            const speed = .2 + ndx * .1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
-        });
-
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(render);
-    }
-
-    requestAnimationFrame(render);
-}
-
-main();
 const EVENTS = [
     /*{
      "location": [{w:0-360deg,h:0-180deg}],     область, где произошло событие; указывается точками, по часовой стрелке
@@ -74,11 +7,11 @@ const EVENTS = [
      "lore_short": "short lore",                краткое описание
      "lore": "full lore about event",           полное поисание
      "tags": []                                 теги; регистр игнорируется
-                                                    *   - тег
-                                                    P_* - человек, относящийся к событию
-                                                    N_* - страна, относящаяся к событию
-                                                    D_* - документ относящийся к событию
-                                                    E_* - другое событие, относящееся к данному
+     *   - тег
+     P_* - человек, относящийся к событию
+     N_* - страна, относящаяся к событию
+     D_* - документ относящийся к событию
+     E_* - другое событие, относящееся к данному
 
      }*/
     {
