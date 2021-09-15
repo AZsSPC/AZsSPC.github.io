65e0a1775bdb272273c2d7f3998f3c5e5a39f0e3
AZ_218 <ratmir.miro@gmail.com>
2021-09-15 20:00:00 +0300

    simplify

--- az.js
+++ az.js
@@ -1,27 +1,53 @@
-function onTextFromCloudLoaded(file, func) {
+function onTextFromCloudLoaded(file, func){
     let rf = new XMLHttpRequest();
     rf.open("GET", file, false);
-    rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ? rf.responseText : null);
+    rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null);
     rf.send();
 }
 
-function runOnKeys(func, ...codes) {
+function runOnKeys(func, ...codes){
     let pressed = new Set();
-    document.addEventListener('keydown', function (event) {
+    document.addEventListener('keydown', function (event){
         pressed.add(event.code);
-        for (let code of codes) if (!pressed.has(code)) return;
+        for(let code of codes) if(!pressed.has(code)) return;
         pressed.clear();
         func();
     });
-    document.addEventListener('keyup', function (event) {
+    document.addEventListener('keyup', function (event){
         pressed.delete(event.code)
     });
 }
 
-function reformatAZ() {
+function reformatAZ(){
     document.getElementsByTagName('main')[0].innerHTML =
         document.getElementsByTagName('main')[0].innerHTML
-            .replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
-            .replace('#projects', '<a href="https://AZsSPC.github.io/projects">projects</a>')
+        .replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
+        .replace('#projects', '<a href="https://AZsSPC.github.io/projects">projects</a>')
     ;
+}
+
+function header(){
+    document.write(
+        '<input type="checkbox" id="navhider" hidden>\n' +
+        '<header>\n' +
+        '    <img id="icon" src="../../img/icon.png" onclick="window.location.href=\'https://AZsSPC.github.io\'"/>\n' +
+        '    <nav>\n' +
+        '        <button onclick="window.location.href=\'https://AZsSPC.github.io/projects\'">projects</button>\n' +
+        '        <button onclick="window.location.href=\'https://AZsSPC.github.io/contacts\'">contacts</button>\n' +
+        '        <button onclick="window.location.href=\'https://AZsSPC.github.io/info\'" class="info-btn" translate="no">info\n' +
+        '        </button>\n' +
+        '    </nav>\n' +
+        '    <label id="hsl" for="navhider"></label>\n' +
+        '</header>'
+    );
+}
+
+function settings(){
+    document.write(
+        '<meta charset="utf-8">\n' +
+        '<meta name="viewport" content="width=device-width,initial-scale=1"/>\n' +
+        '<link rel="icon" href="https://azsspc.github.io/projects/FormulaZ/ic.png">\n' +
+        '<link rel="stylesheet" href="https://azsspc.github.io/main.css">\n' +
+        '<link rel="stylesheet" href="main.css">'
+    );
 }
