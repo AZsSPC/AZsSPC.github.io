6c218ed98e2e1efe0caf24a8825e4f134a91c254
az_218 <ratmir.miro@gmail.com>
2021-10-10 21:31:08 +0300

    FFFFFFFFFff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hi there, i'm AZ!
+>You can find something interesting here
--- az.js
+++ az.js
@@ -1,34 +1,33 @@
-
 //can not be used
-function onTextFromCloudLoaded(file, func){
+function onTextFromCloudLoaded(file, func) {
     let rf = new XMLHttpRequest();
     rf.open("GET", file, false);
-    rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null);
+    rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ? rf.responseText : null);
     rf.send();
 }
 
-function runOnKeys(func, ...codes){
+function runOnKeys(func, ...codes) {
     let pressed = new Set();
-    document.addEventListener('keydown', function (event){
+    document.addEventListener('keydown', function (event) {
         pressed.add(event.code);
-        for(let code of codes) if(!pressed.has(code)) return;
+        for (let code of codes) if (!pressed.has(code)) return;
         pressed.clear();
         func();
     });
-    document.addEventListener('keyup', function (event){
+    document.addEventListener('keyup', function (event) {
         pressed.delete(event.code)
     });
 }
 
-function reformatAZ(){
+function reformatAZ() {
     document.getElementsByTagName('main')[0].innerHTML =
         document.getElementsByTagName('main')[0].innerHTML
-        .replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
-        .replace('#projects', '<a href="https://azsspc.github.io/projects">projects</a>')
+            .replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
+            .replace('#projects', '<a href="https://azsspc.github.io/projects">projects</a>')
     ;
 }
 
-function header(){
+function header() {
     document.write(
         '<input type="checkbox" id="navhider" hidden>\n' +
         '<header>\n' +
@@ -37,14 +36,14 @@ function header(){
         '<button onclick="window.location.href=\'' + (window.location.href.replace(/^.+(github.io)/, 'https://github.com/azsspc/azsspc.github.io/blob/main')) + '\'" class="b-btn">&lt;/&gt;</button>\n' +
         '<button onclick="window.location.href=\'https://azsspc.github.io/projects\'" class="m-btn">projects</button>\n' +
         '<button onclick="window.location.href=\'https://azsspc.github.io/contacts\'">@</button>\n' +
-        '<button onclick="window.location.href=\'https://azsspc.github.io/info\'" translate="no">i</button>\n' +
+        /*'<button onclick="window.location.href=\'https://azsspc.github.io/info\'" translate="no">i</button>\n' +*/
         '</nav>\n' +
         '<label id="hsl" for="navhider"></label>\n' +
         '</header>\n'
     );
 }
 
-function settings(){
+function settings() {
     document.write(
         '<meta charset="utf-8">\n' +
         '<meta name="viewport" content="width=device-width,initial-scale=1"/>\n' +
@@ -54,7 +53,7 @@ function settings(){
     );
 }
 
-function createNDownload(filename = 'AZsSPC.file', text = 'Hello World!'){
+function createNDownload(filename = 'AZsSPC.file', text = 'Hello World!') {
     let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
