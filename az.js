96959be6e846eea205e76d8f0240bb4dc5f29201
az_218 <ratmir.miro@gmail.com>
2021-10-26 12:49:16 +0300

    fixing

--- az.js
+++ az.js
@@ -1,72 +1,58 @@
 //can not be used
 function onTextFromCloudLoaded(file, func){
-    let rf = new XMLHttpRequest();
-    rf.open("GET", file, false);
-    rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null);
-    rf.send();
+	let rf = new XMLHttpRequest();
+	rf.open("GET", file, false);
+	rf.onreadystatechange = func((rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null);
+	rf.send();
 }
 
 function runOnKeys(func, ...codes){
-    let pressed = new Set();
-    document.addEventListener('keydown', function (event){
-        pressed.add(event.code);
-        for(let code of codes) if(!pressed.has(code)) return;
-        pressed.clear();
-        func();
-    });
-    document.addEventListener('keyup', function (event){
-        pressed.delete(event.code)
-    });
+	let pressed = new Set();
+	document.addEventListener('keydown', function(event){
+		pressed.add(event.code);
+		for(let code of codes) if(!pressed.has(code)) return;
+		pressed.clear();
+		func();
+	});
+	document.addEventListener('keyup', function(event){
+		pressed.delete(event.code)
+	});
 }
 
 function reformatAZ(){
-    document.getElementsByTagName('main')[0].innerHTML =
-        document.getElementsByTagName('main')[0].innerHTML
-        .replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
-        .replace('#projects', '<a href="https://azsspc.github.io/projects">projects</a>')
-    ;
+	document.getElementsByTagName('main')[0].innerHTML = document.getElementsByTagName('main')[0].innerHTML.replaceAll('#AZsSPC', '<span title="" translate="no">AZsSPC</span>')
+	.replace('#projects', '<a href="https://azsspc.github.io/projects">projects</a>');
 }
 
 function header(){
-    document.write(
-        '<input type="checkbox" id="navhider" hidden ' + (document.cookie.includes('NAVH=true') ?'checked' :'') + ' onchange="navhider()">\n' +
-        '<header>\n' +
-        '<img id="icon" src="https://azsspc.github.io/img/icon.png" onclick="window.location.href=\'https://azsspc.github.io\'"/>\n' +
-        '<nav>\n' +
-        '<button onclick="window.location.href=\'' + (window.location.href.replace(/^.+(github.io)/, 'https://github.com/azsspc/azsspc.github.io/blob/main')) + '\'" class="b-btn">&lt;/&gt;</button>\n' +
-        '<button onclick="window.location.href=\'https://azsspc.github.io/projects\'" class="m-btn">projects</button>\n' +
-        '<button onclick="window.location.href=\'https://azsspc.github.io/contacts\'">@</button>\n' +
-        /*'<button onclick="window.location.href=\'https://azsspc.github.io/info\'" translate="no">i</button>\n' +*/
-        '</nav>\n' +
-        '<label id="hsl" for="navhider"></label>\n' +
-        '</header>\n'
-    );
+	document.write('<input type="checkbox" id="navhider" hidden ' + (document.cookie.includes('NAVH=true') ?'checked' :'') + ' onchange="navhider()">\n'//
+		+ '<header>\n' + '<img id="icon" src="https://azsspc.github.io/img/icon.png" onclick="window.location.href=\'https://azsspc.github.io\'"/>\n' //
+		+ '<nav>\n'//
+		+ '<a href="'+(window.location.href.replace(/^.+(github.io)/, 'https://github.com/azsspc/azsspc.github.io/blob/main')) + '" class="b-btn">&lt;/&gt;</a>\n'//
+		+ '<a href="https://azsspc.github.io/projects" class="m-btn">projects</a>\n'//
+		+ '<a href="https://azsspc.github.io/contacts">@</a>\n' /*'<button onclick="window.location.href=\'https://azsspc.github.io/info\'" translate="no">i</button>\n' +*///
+		+ '</nav>\n' + '<label id="hsl" for="navhider"></label>\n'//
+		+ '</header>\n');
 }
 
 function settings(){
-    document.write(
-        '<meta charset="utf-8">\n' +
-        '<meta name="viewport" content="width=device-width,initial-scale=1"/>\n' +
-        '<link rel="icon" href="https://azsspc.github.io/img/fic.png">\n' +
-        '<link rel="stylesheet" href="https://azsspc.github.io/main.css">\n' +
-        '<link rel="stylesheet" href="main.css">\n'
-    );
+	document.write('<meta charset="utf-8">\n' + '<meta name="viewport" content="width=device-width,initial-scale=1"/>\n' + '<link rel="icon" href="https://azsspc.github.io/img/fic.png">\n' + '<link rel="stylesheet" href="https://azsspc.github.io/main.JS">\n' + '<link rel="stylesheet" href="main.JS">\n');
 }
 
 function createNDownload(filename = 'AZsSPC.file', text = 'Hello World!'){
-    let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
+	let element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+	element.style.display = 'none';
+	document.body.appendChild(element);
 
-    element.click();
+	element.click();
 
-    document.body.removeChild(element);
+	document.body.removeChild(element);
 }
 
 function navhider(){
-    let nh = document.getElementById('navhider');
-    document.cookie = 'NAVH=' + nh.checked + '; path=/';
+	let nh = document.getElementById('navhider');
+	document.cookie = 'NAVH=' + nh.checked + '; path=/';
 }
--- projects/Web Syntax/main.js
+++ projects/Web Syntax/main.js
@@ -1,101 +1,101 @@
-/** Edit Panes's */ let ep, epf, popup_syntax, popup_upload, epb_reformat, epb_download, epb_syntax, epb_upload, sssb;
+let ep, epf, popup_syntax, popup_upload, epb_reformat, epb_download, epb_syntax, epb_upload, sssb;
 
 const AZR_NB = '[^\\s<>\\[\\]{}();:|\\\\/]', AZR_B = '[\\s<>\\[\\]{}();:|\\\\/]';
-let syntax = {color: [], reform: []};
+let syntax = {color:[], reform:[]};
 let user_syntax = [];
-const basic_syntax = {color: [{p: ".+", r: "red"}], reform: []};
+const basic_syntax = {color:[{p:".+", r:"red",f:"gmi"}], reform:[]};
 const syntax_list = ['JS', 'TASM'];
 
 /** initialization all edit_pane's for future */
 function initEP(){
-    setSyntax(syntax_list[0]);
-    ep = document.getElementById('ep');
-    epf = document.getElementById('epf');
-    popup_syntax = document.getElementById('popup_syntax');
-    popup_upload = document.getElementById('popup_upload');
-    epb_reformat = document.getElementById('edit_pane_refresh');
-    epb_download = document.getElementById('edit_pane_download');
-    epb_upload = document.getElementById('edit_pane_upload');
-    epb_syntax = document.getElementById('edit_pane_syntax');
-    sssb = document.getElementById('sssb');
+	setSyntax(syntax_list[0]);
+	ep = document.getElementById('ep');
+	epf = document.getElementById('epf');
+	popup_syntax = document.getElementById('popup_syntax');
+	popup_upload = document.getElementById('popup_upload');
+	epb_reformat = document.getElementById('edit_pane_refresh');
+	epb_download = document.getElementById('edit_pane_download');
+	epb_upload = document.getElementById('edit_pane_upload');
+	epb_syntax = document.getElementById('edit_pane_syntax');
+	sssb = document.getElementById('sssb');
 
-    popup_syntax.innerHTML = '';
-    for(let i in syntax_list) popup_syntax.innerHTML += '<button onclick="setSyntax(\'' + syntax_list[i] + '\'); popup_syntax.style.display = \'none\'">' + syntax_list[i] + '</button>';
-    ep.setAttribute('oninput', 'onEPI(); epb_reformat.setAttribute("changed","true")');
-    epb_reformat.setAttribute('onclick', 'reformat()');
-    epb_download.setAttribute('onclick', 'downloadThis()');
-    epb_syntax.setAttribute('onclick', 'switchVisible(popup_syntax,\'block\')');
-    epb_upload.setAttribute('onclick', 'switchVisible(popup_upload,\'grid\')');
+	popup_syntax.innerHTML = '';
+	for(let i in syntax_list) popup_syntax.innerHTML += '<button onclick="setSyntax(\'' + syntax_list[i] + '\'); popup_syntax.style.display = \'none\'">' + syntax_list[i] + '</button>';
+	ep.setAttribute('oninput', 'onEPI(); epb_reformat.setAttribute("changed","true")');
+	epb_reformat.setAttribute('onclick', 'reformat()');
+	epb_download.setAttribute('onclick', 'downloadThis()');
+	epb_syntax.setAttribute('onclick', 'switchVisible(popup_syntax,\'block\')');
+	epb_upload.setAttribute('onclick', 'switchVisible(popup_upload,\'grid\')');
 
 //runOnKeys(function () {console.log('pressed')}, "KeyQ", "ControlLeft");
-    runOnKeys(function (){ reformat();}, "AltLeft", "ShiftLeft", "KeyF");
-    runOnKeys(function (){ downloadThis();}, "ControlLeft", "KeyD");
+	runOnKeys(function(){ reformat();}, "AltLeft", "ShiftLeft", "KeyF");
+	runOnKeys(function(){ downloadThis();}, "ControlLeft", "KeyD");
 
-    onEPI(document.getElementById(epb_reformat.getAttribute('for')));
+	onEPI(document.getElementById(epb_reformat.getAttribute('for')));
 }
 
 /** on [Edit Pane Input] detected */
 function onEPI(){
-    if(ep.innerHTML.indexOf('<li>') !== 0) ep.innerHTML = '</li><li>' + ep.innerText;
-    let text = ep.innerHTML.replaceAll(/<li>/gm, '\n').replaceAll(/<.+?>/gm, '').replaceAll(/&nbsp;/gm, ' ');
-    for(let i in syntax.color) text = text.replaceAll(syntax.color[i].p, '<span class="' + syntax.color[i].r + '">$&</span>');
-    //user_syntax.forEach(function (a){text = text.replaceAll(a.p, a.replace)});
-    epf.innerHTML = ('<li>' + text.replaceAll(/\n/gm, '</li><li>') + '</li>').substr(9);
+	if(ep.innerHTML.indexOf('<li>') !== 0) ep.innerHTML = '</li><li>' + ep.innerText;
+	let text = ep.innerHTML.replaceAll(/<li>/gm, '\n').replaceAll(/<.+?>/gm, '').replaceAll(/&nbsp;/gm, ' ');
+	for(let i in syntax.color) text = text.replaceAll(syntax.color[i].p, '<span class="' + syntax.color[i].r + '">$&</span>');
+	//user_syntax.forEach(function (a){text = text.replaceAll(a.p, a.replace)});
+	epf.innerHTML = ('<li>' + text.replaceAll(/\n/gm, '</li><li>') + '</li>').substr(9);
 }
 
 /** reformat */
 function reformat(){
-    console.log('refresh');
+	console.log('refresh');
 
-    if(ep.innerHTML.indexOf('<li>') !== 0) ep.innerHTML = '</li><li>' + ep.innerText;
-    let text = ep.innerHTML.replaceAll(/<li>/gm, '\n').replaceAll(/<.+?>/gm, '').replaceAll(/&nbsp;/gm, ' ');
-    syntax.reform.forEach(function (a){text = text.replaceAll(a.p, a.r)});
-    ep.innerHTML = ('<li>' + text.replaceAll(/\n/gm, '</li><li>') + '</li>').substr(9);
+	if(ep.innerHTML.indexOf('<li>') !== 0) ep.innerHTML = '</li><li>' + ep.innerText;
+	let text = ep.innerHTML.replaceAll(/<li>/gm, '\n').replaceAll(/<.+?>/gm, '').replaceAll(/&nbsp;/gm, ' ');
+	syntax.reform.forEach(function(a){text = text.replaceAll(a.p, a.r)});
+	ep.innerHTML = ('<li>' + text.replaceAll(/\n/gm, '</li><li>') + '</li>').substr(9);
 
-    onEPI();
-    epb_reformat.setAttribute("changed", "false")
+	onEPI();
+	epb_reformat.setAttribute("changed", "false")
 }
 
 function downloadThis(){
-    let filename = prompt("What do you want to name the file?");
-    if(filename) createNDownload(filename, ep.innerText);
+	let filename = prompt("What do you want to name the file?");
+	if(filename) createNDownload(filename, ep.innerText);
 }
 
 function setSyntax(syntax_name){
-    let rf = new XMLHttpRequest();
-    rf.open("GET", 'https://raw.githubusercontent.com/AZsSPC/AZsSPC.github.io/main/projects/Web%20Syntax/syntax/json/' + syntax_name + '.json');
-    rf.onreadystatechange = function (){
-        let text = (rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null;
-        syntax = text ?JSON.parse(text) :basic_syntax;
-        for(let i in syntax.color) syntax.color[i].p = new RegExp(syntax.color[i].p, syntax.color[i].f ?? 'gmi');
-        for(let i in syntax.reform) syntax.reform[i].p = new RegExp(syntax.reform[i].p, syntax.reform[i].f ?? 'gmi');
-        sssb.href = 'syntax/css/' + syntax_name + '.css';
-        onEPI();
-    }
-    rf.send();
+	let rf = new XMLHttpRequest();
+	rf.open("GET", 'https://raw.githubusercontent.com/AZsSPC/AZsSPC.github.io/main/projects/Web%20Syntax/syntax/' + syntax_name + '/s.json');
+	rf.onreadystatechange = function(){
+		let text = (rf.readyState === 4 && (rf.status === 200 || rf.status == null)) ?rf.responseText :null;
+		syntax = text ?JSON.parse(text) :basic_syntax;
+		for(let i in syntax.color) syntax.color[i].p = new RegExp(syntax.color[i].p, syntax.color[i].f ?? 'gmi');
+		for(let i in syntax.reform) syntax.reform[i].p = new RegExp(syntax.reform[i].p, syntax.reform[i].f ?? 'gmi');
+		sssb.href = 'syntax/' + syntax_name + '/s.JS';
+		onEPI();
+	}
+	rf.send();
 }
 
 function fileUploaded(el){
-    let file = el.files[0] ?? null;
-    console.log(file);
-    if(file){
-        let reader = new FileReader();
-        reader.readAsText(file, "UTF-8");
-        reader.onload = function (evt){
-            popup_upload.style.display = 'none';
-            ep.innerText = evt.target.result;
-            onEPI();
-        }
-        reader.onerror = function (evt){
-            ep.innerText = 'error while read file';
-            onEPI();
-        }
-    }
+	let file = el.files[0] ?? null;
+	console.log(file);
+	if(file){
+		let reader = new FileReader();
+		reader.readAsText(file, "UTF-8");
+		reader.onload = function(evt){
+			popup_upload.style.display = 'none';
+			ep.innerText = evt.target.result;
+			onEPI();
+		}
+		reader.onerror = function(evt){
+			ep.innerText = 'error while read file';
+			onEPI();
+		}
+	}
 
 }
 
 function switchVisible(el, v){
-    el.style.display = (el.style.display === v ?'none' :v);
+	el.style.display = (el.style.display === v ?'none' :v);
 }
 
 initEP();
