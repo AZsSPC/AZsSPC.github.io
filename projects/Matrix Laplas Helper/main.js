9a80574765c6fe9f056e2c59c3602f406a455189
AZ_218 <ratmir.miro@gmail.com>
2021-12-11 13:49:55 +0200

    Matrix Laplas Helper in development
    rov actions allow!

--- projects/Matrix Laplas Helper/main.js
+++ projects/Matrix Laplas Helper/main.js
@@ -1,50 +1,85 @@
-const max_val = 10;
-let m_size = 4;
-let tab = document.getElementById("as");
-let matrix;
+let startMatrix, currentMatrix, width = 0, height = 0, commands;
+let mW = document.getElementById('mW'), mH = document.getElementById('mH'),
+    matrixTable = document.getElementById('matrixTable'),
+    initMS = document.getElementById('initMS'),
+    stepCommand = document.getElementById('stepCommand'),
+    initMatrixc = document.getElementById('initMatrix'),
+    stepMatrixc = document.getElementById('stepMatrix');
 
-function recreate(){
-    matrix = new Array(m_size);
-    tab.innerHTML = m_size + "*" + m_size + "\n";
-    for(let r = 0; r < m_size; r++){
-        matrix[r] = new Array(m_size);
-        for(let c = 0; c < m_size; c++){
-            matrix[r][c] = Math.floor((Math.random() * 2 - 1) * max_val);
-            tab.innerHTML += matrix[r][c] + "\t";
-        }
-        tab.innerHTML += "\n";
+function initMatrixSize(){
+    initMS.hidden = true;
+    initMatrixc.hidden = false;
+    stepMatrixc.hidden = true;
+    width = mW.value;
+    height = mH.value;
+    let mt = '';
+    for(let h = 0; h < height; h++){
+        mt += '<tr>';
+        for(let w = 0; w < width; w++) mt += '<td id="me_i' + w + '_j' + h + '" contenteditable="true">0</td>';
+        mt += '</tr>';
     }
-    tab.innerHTML += "\ndelta: " + Determinant(matrix);
+    matrixTable.innerHTML = mt;
 }
 
-function Determinant(A){
-    let m_size = A.length, B = [], denom = 1, exchanges = 0;
-    for(let i = 0; i < m_size; ++i){
-        B[i] = [];
-        for(let j = 0; j < m_size; ++j) B[i][j] = A[i][j];
+function stepMatrix(){
+    let command = stepCommand.value.replaceAll(/\s/g, '').toLowerCase().match(/([rc])(\d+)([\/\-+>*])(-?\d+)/);
+    stepCommand.value = '';
+    command[2] = parseInt(command[2]) - 1;
+    command[4] = parseInt(command[4]);
+    console.log(command)
+    if(command.length !== 5) return;
+    commands.push(command[0]);
+    if(command[1] === 'r') switch(command[3]){
+        case '-':
+            currentMatrix[command[2]] = currentMatrix[command[2]].map((a, i) => a - currentMatrix[command[4] - 1][i]);
+            break;
+        case '+':
+            currentMatrix[command[2]] = currentMatrix[command[2]].map((a, i) => a + currentMatrix[command[4] - 1][i]);
+            break;
+        case '*':
+            currentMatrix[command[2]] = currentMatrix[command[2]].map((a, i) => a * command[4]);
+            break;
+        case '/':
+            currentMatrix[command[2]] = currentMatrix[command[2]].map((a, i) => a / command[4]);
+            break;
+        case '>':
+            currentMatrix.splice(command[4] - 1, 0, currentMatrix.splice(command[2], 1)[0]);
+            break;
+    } else switch(command[3]){
+        case '-':
+        case '+':
+        case '*':
+        case '/':
+        case '>':
     }
-    for(let i = 0; i < m_size - 1; ++i){
-        let maxN = i, maxValue = Math.abs(B[i][i]);
-        for(let j = i + 1; j < m_size; ++j){
-            let value = Math.abs(B[j][i]);
-            if(value > maxValue){
-                maxN = j;
-                maxValue = value;
-            }
+    for(let h = 0; h < height; h++) for(let w = 0; w < width; w++) document.getElementById('me_i' + w + '_j' + h).innerText = currentMatrix[h][w];
+
+}
+
+function initMatrix(){
+    initMatrixc.hidden = true;
+    stepMatrixc.hidden = false;
+    commands = new Array(0);
+    startMatrix = new Array(height);
+    for(let h = 0; h < height; h++){
+        startMatrix[h] = new Array(width);
+        for(let w = 0; w < width; w++){
+            let value = document.getElementById('me_i' + w + '_j' + h).innerText.replaceAll(/[^\d-]/g, '').replaceAll(/(?<=.)-/g, '');
+            startMatrix[h][w] = value.length > 0 ?parseInt(value) :0;
         }
-        if(maxN > i){
-            let temp = B[i];
-            B[i] = B[maxN];
-            B[maxN] = temp;
-            ++exchanges;
-        }else if(maxValue === 0) return 0;
-        let value1 = B[i][i];
-        for(let j = i + 1; j < m_size; ++j){
-            let value2 = B[j][i];
-            B[j][i] = 0;
-            for(let k = i + 1; k < m_size; ++k) B[j][k] = (B[j][k] * value1 - B[i][k] * value2) / denom;
-        }
-        denom = value1;
     }
-    return exchanges % 2 ?-B[m_size - 1][m_size - 1] :B[m_size - 1][m_size - 1];
+    currentMatrix = matrixClone(startMatrix);
+}
+
+function matrixClone(oldM){
+    let mh = oldM.length;
+    let mw = oldM[0].length;
+    let newM = new Array(mh);
+    for(let h = 0; h < mh; h++){
+        newM[h] = new Array(mw);
+        for(let w = 0; w < mw; w++)
+            newM[h][w] = oldM[h][w];
+    }
+    console.log(newM);
+    return newM;
 }
