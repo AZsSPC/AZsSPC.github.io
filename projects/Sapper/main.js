632250652c38e886fdf64c0b673b36e3ff29dfd4
az_218 <ratmir.miro@gmail.com>
2021-12-26 01:14:26 +0300

    Turing Machine may done

--- projects/Sapper/main.js
+++ projects/Sapper/main.js
@@ -2,88 +2,111 @@ const gc = 21;
 const BOMB = -1;
 const FLAG = -2;
 const EMPTY = 0;
-let game;
+let game= document.getElementById('game');
 let tiles = [];
 let search = [-1, 1, gc - 1, gc + 1, -1 - gc, 1 - gc, gc, -gc, 0];
 
 function setup(){
-    game = document.getElementById('game');
-    tiles = new Array(gc ** 2);
-    for(let x = 0; x < gc ** 2; x++){
-        let tile = document.createElement('span');
-        tile.setAttribute('onclick', 'find(' + x + ')');
-        tiles[x] = {v: (Math.random() * 100 < 10 ?BOMB :EMPTY), c: false};
-        game.append(tile);
-    }
-    for(let x = 0; x < gc ** 2; x++)
-        if(tiles[x].v !== BOMB){
-            let b = 0;
-            for(let i = 0; i < 8; i++){
-                let pos = x + search[i];
-                if(pos >= 0 && pos < gc ** 2 && tiles[pos].v === BOMB) b++;
-            }
-            tiles[x].v = b;
-        }
+	tiles = new Array(gc ** 2);
+	game.innerText = '';
+	for(let x = 0; x < gc ** 2; x++){
+		let tile = document.createElement('span');
+		tile.setAttribute('onclick', 'find(' + x + ')');
+		tile.setAttribute('oncontextmenu', 'mark(' + x + ');return false');
+		tiles[x] = {
+			v:EMPTY,
+			c:false
+		};
+		game.append(tile);
+	}
+	let bombs_left = (gc ** 2 * .218).toFixed();
+	while(bombs_left > 0){
+		console.log(bombs_left)
+		let r = (Math.random() * (gc ** 2)).toFixed();
+		if(tiles[r].v === EMPTY){
+			tiles[r].v = BOMB;
+			bombs_left--
+		}
+	}
+	for(let x = 0; x < gc ** 2; x++) if(tiles[x].v !== BOMB){
+		let b = 0;
+		for(let i = 0; i < 8; i++){
+			let pos = x + search[i];
+			if(pos >= 0 && pos < gc ** 2 && tiles[pos].v === BOMB){
+				b++;
+			}
+		}
+		tiles[x].v = b;
+	}
 }
 
 function gmc(id = 0){
-    switch(id){
-        default:
-            return '#797979';
-        case 0:
-            return '#d7d7d7';
-        case 1:
-            return '#2f4de7';
-        case 2:
-            return '#1ec205';
-        case 3:
-            return '#de1c59';
-        case 4:
-            return '#8d0de0';
-        case 5:
-            return '#e88d00';
-        case 6:
-            return '#cd4ead';
-        case 7:
-            return '#8daf13';
-        case 8:
-            return '#007d02';
-        case BOMB:
-            return '#000000';
-        case FLAG:
-            return '#af8989';
-    }
+	switch(id){
+	default:
+		return '#797979';
+	case 0:
+		return '#B2B2B2';
+	case 1:
+		return '#2F4DE7';
+	case 2:
+		return '#1EC205';
+	case 3:
+		return '#DE1C59';
+	case 4:
+		return '#8D0DE0';
+	case 5:
+		return '#E88D00';
+	case 6:
+		return '#CD4EAD';
+	case 7:
+		return '#8DAF13';
+	case 8:
+		return '#007D02';
+	case BOMB:
+		return '#000000';
+	case FLAG:
+		return '#FFFFFF';
+	}
 }
 
 function gms(id = 0){
-    switch(id){
-        default:
-            return id + '';
-        case 0:
-            return '';
-        case BOMB:
-            return '*';
-        case FLAG:
-            return '?';
-    }
+	switch(id){
+	default:
+		return id + '';
+	case 0:
+		return '';
+	case BOMB:
+		return '*';
+	case FLAG:
+		return '?';
+	}
 }
 
 function set(id){
-    let el = game.children[id];
-    tiles[id].c = true;
-    el.innerText = gms(tiles[id].v);
-    el.style.setProperty('background', gmc(tiles[id].v));
-    el.setAttribute('onclick', '');
+	let el = game.children[id];
+	tiles[id].c = true;
+	el.innerText = gms(tiles[id].v);
+	el.style.setProperty('background', gmc(tiles[id].v));
+	el.removeAttribute('onclick');
+	if(tiles[id].v === BOMB){ alert('You lose'); }
+}
+
+function mark(id){
+	let el = game.children[id];
+	el.innerText = gms(FLAG);
+	el.style.setProperty('background', gmc(FLAG));
 }
 
 function find(id){
-    for(let i = 0; i < 9; i++){
-        let pos = id + search[i];
-        if(pos >= 0 && pos < gc ** 2 && tiles[pos].v !== BOMB){
-            if(tiles[pos].v !== EMPTY && !tiles[pos].c) set(pos);
-            if(tiles[pos].v === EMPTY) find(pos);
-        }
-    }
+	if(!tiles[id].c){
+		set(id);
+		if(tiles[id].v === EMPTY){
+			for(let i = 0; i < 9; i++){
+				let pos = id + search[i];
+				if(pos >= 0 && pos < gc ** 2 && !tiles[pos].c){ setTimeout(() => find(pos), 0); }
+			}
+		}
+	}
 }
 
 setup();
