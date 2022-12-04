a82a22519001b4dcd5692e15025a72fb6e1cc2d4
AZsSPC <Vjycnh13!az>
2022-12-04 18:57:23 +0200

    Alchemy

--- projects/Alchemy/main.js
+++ projects/Alchemy/main.js
@@ -1,21 +1,17 @@
+const GAME = document.getElementById('alchemy')
+
 const Ingredient = class Ingredient {
-    constructor(title, glyph, fg, bg, lore) {
+    constructor(title, fg, bg, lore) {
         this.title = title
-        this.glyph = glyph
         this.fg = fg
         this.bg = bg
         this.lore = lore
     }
 
-    get display_glyph() {
-        return `<span class="glyph" style="color:${this.fg};background:${this.bg}">${this.glyph}</span>`
-    }
-
     static list = {}
 
-    static init(id, title, glyph, fg, bg, lore) {
-        Ingredient.list[id] = new Ingredient(title, glyph, fg, bg, lore)
-        return id
+    static init(id, title, fg, bg, lore) {
+        Ingredient.list[id] = new Ingredient(title, fg, bg, lore)
     }
 }
 
@@ -26,10 +22,6 @@ const RecipeIngredient = class RecipeIngredient {
         this.tmax = tmax
         this.amin = amin
         this.amax = amax
-    }
-
-    check(t, c) {
-        return this.tmin <= t && this.tmax >= t && this.amin <= c && this.amax >= c
     }
 }
 
@@ -87,29 +79,54 @@ const Brew = class Brew {
     }
 }
 
-const init_ingredients_and_recipes = () => {
-    /* FUNDAMENTAL */
-    Ingredient.init("water", "Water", "W", "#8998af", "#000000", "Water from the forest river")
-    Ingredient.init("cave_shrooms", "Cave shooms", "T", "#fabcaa", "#000000", "Shrooms wich grows near the caves")
-    /* CRAFTABLE */
-    Ingredient.init("shroom_poison", "Shroom poison", "U", "#16ff34", "#000000", "Poisonous shrooms boiled in water")
+//
 
-    Recipe.init("shroom_poison", 1,
-        new RecipeIngredient("water", 90, 150, 60, 70),
-        new RecipeIngredient("cave_shrooms", 20, 80, 30, 40))
-    Recipe.init("shroom_soup", 1,
-        new RecipeIngredient("cave_shrooms", 20, 80, 45, 55),
-        new RecipeIngredient("water", 90, 150, 45, 55))
+/* FUNDAMENTAL */
+Ingredient.init('water', 'Water', '#c3f1ff', '#2581b9', 'Water from the forest river')
+Ingredient.init('cave_shrooms', 'Cave shooms', '#fabcaa', '#57524b', 'Shrooms wich grows near the caves')
+/* CRAFTABLE */
+Ingredient.init('shroom_poison', 'Shroom poison', '#16ff34', '#22573f', 'Poisonous shrooms boiled in water')
+
+Recipe.init('shroom_poison', 1,
+    new RecipeIngredient('water', 90, 150, 60, 70),
+    new RecipeIngredient('cave_shrooms', 20, 80, 30, 40))
+Recipe.init('shroom_soup', 1,
+    new RecipeIngredient('cave_shrooms', 20, 80, 45, 55),
+    new RecipeIngredient('water', 90, 150, 45, 55))
+
+//
+
+const inventory = (() => {
+    const inv = document.createElement('table')
+    inv.className = 'inventory'
+    inv.slots = Ingredient.list
+    inv.refresh = () => {
+        inv.innerHTML = '<tr><td>Title</td><td>Lore</td><</tr>'
+        for (let id in inv.slots) {
+            const e = Ingredient.list[id]
+            inv.innerHTML += `<span class="glyph" style="color:${e.fg};background:${e.bg}" title="${e.title}\n${e.lore}">${e.title}</span>`
+        }
+    }
+    inv.refresh()
+    return inv
+})()
+
+const Slot = class Slot extends HTMLDivElement {
+    constructor() {
+        super()
+        this.className = 'slot'
+        this.item = null
+    }
 }
 
 //
 
-init_ingredients_and_recipes()
-
-const GAME = document.getElementById("alchemy")
-GAME.innerHTML = ""
-
+//Object.keys(Ingredient.list).length * 2 - 2)
+GAME.innerHTML = ''
+GAME.append(inventory)
+/*
 let brew = new Brew()
 brew.add(new BrewingIngredient('water', 100, 70))
 brew.add(new BrewingIngredient('cave_shrooms', 50, 35))
 console.log(brew.brew())
+ */
