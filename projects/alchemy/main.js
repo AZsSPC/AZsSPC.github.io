8d194adc8fb7cb87c28cc6799d0a333359417ca6
Ratmyr aka AZ <Ratmir.miro@gmail.com>
2024-12-03 06:59:10 +0200

    Behold! Working on alchemy in 4D

--- projects/alchemy/main.js
+++ projects/alchemy/main.js
@@ -1,132 +1,219 @@
-const GAME = document.getElementById('alchemy')
+import * as THREE from 'https://unpkg.com/three@v0.160.0/build/three.module.js'
+import {Vector2, Vector4} from 'https://unpkg.com/three@v0.160.0/build/three.module.js'
 
-const Ingredient = class Ingredient {
-    constructor(title, fg, bg, lore) {
-        this.title = title
-        this.fg = fg
-        this.bg = bg
-        this.lore = lore
-    }
+class Ingredient {
+	constructor(vector_position = new Vector4(), vector_weight = new Vector4(), vector_radius = new Vector2(), elements = {}, title = 'brew.unknown.title', lore = 'brew.unknown.lore', color = 0xffffff, amount = 1) {
+		this.vector_position = vector_position
+		this.vector_weight = vector_weight
+		this.vector_radius = vector_radius
+		this.elements = elements
+		this.title = title
+		this.lore = lore
+		this.color = color
+		this.amount = amount
+	}
 
-    static list = {}
+	static list = Object.freeze({
+		water: new Ingredient(new Vector4(0, 0, 0, 0), new Vector4(1, 1, 1, 0), new Vector2(7, 1),
+			{}, 'ingredient.water.title', 'ingredient.water.lore', 0x0000ff),
+		herb: new Ingredient(new Vector4(-100, 0, 10, 0), new Vector4(1, 1, 1, 0), new Vector2(10, 1),
+			{}, 'ingredient.herb.title', 'ingredient.herb.lore', 0x00ff00),
+		blood: new Ingredient(new Vector4(10, 50, 10, 0), new Vector4(1, 1, 1, 0), new Vector2(10, 1),
+			{}, 'ingredient.herb.title', 'ingredient.herb.lore', 0xff0000),
+		wood: new Ingredient(new Vector4(-10, -80, 30, 0), new Vector4(1, 1, 1, 0), new Vector2(2, 1),
+			{}, 'ingredient.herb.title', 'ingredient.herb.lore', 0xa40910),
+		wood1: new Ingredient(new Vector4(50, -8, -60, 0), new Vector4(1, 1, 1, 0), new Vector2(3, 1),
+			{}, 'ingredient.herb.title', 'ingredient.herb.lore', 0xa40910),
+		wood2: new Ingredient(new Vector4(-20, 70, -5, 0), new Vector4(1, 1, 1, 0), new Vector2(3, 1),
+			{}, 'ingredient.herb.title', 'ingredient.herb.lore', 0xa40910),
+	})
 
-    static init(id, title, fg, bg, lore) {
-        Ingredient.list[id] = new Ingredient(title, fg, bg, lore)
-    }
+	copy(amount = this.amount) {
+		return new Ingredient(this.vector_position, this.vector_weight, this.vector_radius, this.elements, this.title, this.lore, this.color, amount)
+	}
 }
 
-const RecipeIngredient = class RecipeIngredient {
-    constructor(id, tmin, tmax, amin, amax) {
-        this.id = id
-        this.tmin = tmin
-        this.tmax = tmax
-        this.amin = amin
-        this.amax = amax
-    }
+class Brew {
+	constructor() {
+		this.vector_position = new Vector4(0, 0, 0, 0)
+		this.vector_weight = new Vector4(0, 0, 0, 0)
+		this.amount = 0
+		this.elements = {}
+	}
+
+	put(ingredient) {
+		const total_weight = new THREE.Vector4(
+			this.vector_weight.x * this.amount + ingredient.vector_weight.x * ingredient.amount,
+			this.vector_weight.y * this.amount + ingredient.vector_weight.y * ingredient.amount,
+			this.vector_weight.z * this.amount + ingredient.vector_weight.z * ingredient.amount,
+			this.vector_weight.w * this.amount + ingredient.vector_weight.w * ingredient.amount,
+		)
+
+		const combined_amount = this.amount + ingredient.amount
+		const normalized_weight = new THREE.Vector4(
+			total_weight.x / combined_amount,
+			total_weight.y / combined_amount,
+			total_weight.z / combined_amount,
+			total_weight.w / combined_amount,
+		)
+
+		this.vector_position.x += (ingredient.vector_position.x - this.vector_position.x) * normalized_weight.x
+		this.vector_position.y += (ingredient.vector_position.y - this.vector_position.y) * normalized_weight.y
+		this.vector_position.z += (ingredient.vector_position.z - this.vector_position.z) * normalized_weight.z
+		this.vector_position.w += (ingredient.vector_position.w - this.vector_position.w) * normalized_weight.w
+
+		this.vector_weight = normalized_weight
+		this.amount = combined_amount
+
+		Object.entries(ingredient.elements).forEach(([k, v]) => { this.elements[k] = (this.elements[k] || 0) + v })
+	}
+
+	take() {
+		const result = new Ingredient(this.vector_position, this.vector_weight, new Vector2(), this.elements, 'ingredient.test.title', 'ingredient.test.lore', 0x555, this.amount)
+		this.vector_position = new Vector4(0, 0, 0, 0)
+		this.vector_weight = new Vector4(0, 0, 0, 0)
+		this.amount = 0
+		this.elements = {}
+		return result
+	}
 }
 
-const BrewingIngredient = class BrewingIngredient {
-    constructor(id, temperature, amount) {
-        this.id = id
-        this.temperature = temperature
-        this.amount = amount
-    }
+class Deadzone {
+	constructor(states, connections) {
+		this.states = states
+		this.connections = connections
+		this.material = new THREE.MeshStandardMaterial({
+			color: 0xffffff,
+			roughness: 0,
+			metalness: 0,
+			opacity: 0.5,
+			side: THREE.DoubleSide,
+			transparent: true,
+		})
+		this.geometry = new THREE.BufferGeometry()
+		this.mesh = new THREE.Mesh(this.geometry, this.material)
+		this.updateGeometry(0)
+	}
+
+	updateGeometry(w_slider) {
+		const vertices = this.states.flatMap(v => this.findVertex(v, w_slider))
+		const faces = this.connections.flatMap(face => face)
+		this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+		this.geometry.setIndex(faces)
+		this.geometry.computeVertexNormals()
+	}
+
+	findVertex(arrays, d4) {
+		const n = arrays.length - 1
+		if (d4 <= arrays[0][3]) return arrays[0].slice(0, 3)
+		if (d4 >= arrays[n][3]) return arrays[n].slice(0, 3)
+
+		for (let i = 0; i < n; i++) {
+			const [v1, v2] = [arrays[i], arrays[i + 1]]
+			if (d4 < v1[3] || d4 >= v2[3]) continue
+			const weight = (d4 - v1[3]) / (v2[3] - v1[3])
+			return v1.map((v, j) => v * (1 - weight) + v2[j] * weight).slice(0, 3)
+		}
+		return [0, 0, 0]
+	}
+
 }
 
-const Recipe = class Recipe {
-    constructor(id, actual_product_percent, ...recipe_ingredients) {
-        this.product = id
-        this.ingredients = []
-        recipe_ingredients.forEach(e => this.ingredients.push(e))
-    }
+const brew = new Brew()
+brew.put(Ingredient.list.herb.copy(9))
+brew.put(Ingredient.list.wood.copy(5))
+brew.put(Ingredient.list.blood.copy(2))
+//brew.take()
 
-    static list = []
+const map_div = document.getElementById('map')
+let {width: vw, height: vh} = map_div.getBoundingClientRect()
 
-    static init(id, actual_product_percent, ...recipe_ingredients) {
-        Recipe.list.push(new Recipe(id, actual_product_percent, ...recipe_ingredients))
-    }
+const scene = new THREE.Scene()
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(vw, vh)
+map_div.appendChild(renderer.domElement)
+
+let camera = new THREE.PerspectiveCamera(75, vw / vh, 0.1, 1000)
+camera.position.z = 200
+
+const group = new THREE.Group()
+scene.add(group)
+const groupsphere = new THREE.Group()
+scene.add(groupsphere)
+
+const createIngredientSpheres = () => {
+	// Remove previous ingredients
+	//groupsphere.children.forEach(child => child instanceof THREE.Mesh && groupsphere.remove(child))
+	groupsphere.clear()
+
+	Object.entries(Ingredient.list).forEach(([k, v]) => {
+		if (Math.abs(v.vector_position.w - brew.vector_position.w) > v.vector_radius.y) return
+
+		const geometry = new THREE.SphereGeometry(v.vector_radius.x * (1 - Math.abs(v.vector_position.w - brew.vector_position.w) / v.vector_radius.y))
+		const material = new THREE.MeshStandardMaterial({color: v.color, roughness: 0.7, metalness: 0.2})
+		const sphere = new THREE.Mesh(geometry, material)
+		sphere.position.set(v.vector_position.x, v.vector_position.y, v.vector_position.z)
+		groupsphere.add(sphere)
+	})
 }
 
-const Brew = class Brew {
-    constructor() {
-        this.full_amount = 0
-        this.ingredients = []
-    }
+const deadzones = [
+	new Deadzone([
+		[[50, 0, 0, 0], [50, 10, 20, 0.5], [40, -6, 5, 1]],
+		[[50, 50, 50, 0], [30, 60, 60, 0.5], [30, 10, 40, 1]],
+		[[0, 0, 0, 0], [10, 10, 10, 0.5], [23, 7, 5, 1]],
+		[[50, 50, -20, 0], [60, 40, 10, 0.5], [-20, 10, -10, 1]],
+	], [[0, 1, 2], [0, 2, 3], [0, 1, 3], [1, 2, 3]]),
 
-    add(brewing_ingredient) {
-        this.ingredients.push(brewing_ingredient)
-    }
+	new Deadzone([
+		[[0, -50, -50, 0], [-23, -30, -10, 0.5]],
+		[[-50, 0, 0, 0.5], [10, -6, -5, 1]],
+		[[-50, -50, 0, 0], [-40, -20, -10, 0.8]],
+		[[-50, 0, -50, 0.2], [-10, 20, -40, 1]],
+	], [[0, 1, 2], [0, 2, 3], [0, 1, 3], [1, 2, 3]]),
+]
 
-    is_suitable_recipe(recipe) {
-        for (let i in recipe.ingredients) {
-            const a = this.ingredients[i], b = this.ingredients[i]
-            if (a.id !== b.id &&
-                b.tmin <= a.temperature <= b.tmax &&
-                b.amin <= a.amount / this.full_amount <= b.amax
-            ) return false
-        }
-        return true
-    }
+deadzones.forEach(e => group.add(e.mesh))
 
-    brew() {
-        this.full_amount = 0
-        this.ingredients.forEach(e => this.full_amount += e.amount)
-        for (const recipe of Recipe.list)
-            if (this.ingredients.length === this.ingredients.length)
-                if (this.is_suitable_recipe(recipe))
-                    return recipe
-        return false
-    }
-}
+const light = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(light)
 
-//
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
+directionalLight.position.set(35, 50, 75)
+scene.add(directionalLight)
 
-/* FUNDAMENTAL */
-Ingredient.init('water', 'Water', '#c3f1ff', '#2581b9', 'Water from the forest river')
-Ingredient.init('cave_shrooms', 'Cave shooms', '#fabcaa', '#57524b', 'Shrooms wich grows near the caves')
-/* CRAFTABLE */
-Ingredient.init('shroom_poison', 'Shroom poison', '#16ff34', '#22573f', 'Poisonous shrooms boiled in water')
 
-Recipe.init('shroom_poison', 1,
-    new RecipeIngredient('water', 90, 150, 60, 70),
-    new RecipeIngredient('cave_shrooms', 20, 80, 30, 40))
-Recipe.init('shroom_soup', 1,
-    new RecipeIngredient('cave_shrooms', 20, 80, 45, 55),
-    new RecipeIngredient('water', 90, 150, 45, 55))
+let rotationAngle = 0
+let f = false
 
-//
-
-const inventory = (() => {
-    const inv = document.createElement('table')
-    inv.className = 'inventory'
-    inv.slots = Ingredient.list
-    inv.refresh = () => {
-        inv.innerHTML = '<tr><td>Title</td><td>Lore</td><</tr>'
-        for (let id in inv.slots) {
-            const e = Ingredient.list[id]
-            inv.innerHTML += `<span class="glyph" style="color:${e.fg};background:${e.bg}" title="${e.title}\n${e.lore}">${e.title}</span>`
-        }
-    }
-    inv.refresh()
-    return inv
-})()
-
-const Slot = class Slot extends HTMLDivElement {
-    constructor() {
-        super()
-        this.className = 'slot'
-        this.item = null
-    }
-}
-
-//
-
-//Object.keys(Ingredient.list).length * 2 - 2)
-GAME.innerHTML = ''
-GAME.append(inventory)
 /*
-let brew = new Brew()
-brew.add(new BrewingIngredient('water', 100, 70))
-brew.add(new BrewingIngredient('cave_shrooms', 50, 35))
-console.log(brew.brew())
+ scene.add(new THREE.Mesh(new THREE.SphereGeometry(150,20,20), new THREE.MeshStandardMaterial(
+ {color: 0xffffff, roughness: 0.7, metalness: 0.2, opacity: 0.2, transparent: true},
+ )))
  */
+
+function animate() {
+	const {width: nvw, height: nvh} = map_div.getBoundingClientRect()
+
+	if (vw !== nvw || vh !== nvh) {
+		[vw, vh] = [nvw, nvh]
+		renderer.setSize(vw, vh)
+		camera.aspect = vw / vh
+		camera.clearViewOffset()
+	}
+
+	requestAnimationFrame(animate)
+
+	rotationAngle += 0.01
+
+	group.rotation.y = rotationAngle
+	groupsphere.rotation.y = rotationAngle
+
+	createIngredientSpheres()
+	deadzones.forEach(e => e.updateGeometry(brew.vector_position.w))
+
+	renderer.render(scene, camera)
+}
+
+animate()
--- projects/main.js
+++ projects/main.js
@@ -17,141 +17,141 @@ const PROJECTS = [
 		name: 'Alchemy',
 		path: 'alchemy',
 		lore: 'My interpretation of the alchemical games',
-		enabled: true, weight: -1,
-		tag: [TAG.WEB, TAG.GAME, TAG.FOR_ALL]
+		enabled: true, weight: 0,
+		tag: [TAG.WEB, TAG.GAME, TAG.FOR_ALL, TAG.MATH, TAG.SCIENCE],
 	},
 	{
 		name: 'AZLang',
 		path: 'az_lang',
 		lore: '',
 		enabled: false,
-		tag: [TAG.WEB]
+		tag: [TAG.WEB],
 	},
 	{
 		name: 'azembly',
 		path: 'azembly',
 		lore: '',
 		enabled: false,
-		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.PROGRAMMING]
+		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.PROGRAMMING],
 	},
 	{
 		name: 'block_schemes',
 		path: 'block_schemes',
 		lore: '',
 		enabled: false,
-		tag: [TAG.WEB]
+		tag: [TAG.WEB],
 	},
 	{
 		name: 'electrical_board',
 		path: 'electrical_board',
 		lore: '',
 		enabled: false,
-		tag: [TAG.WEB]
+		tag: [TAG.WEB],
 	},
 	{
 		name: 'Evo',
 		path: 'evo',
 		lore: 'Primitive evolution simulator developed in 6h [Updating]',
 		enabled: true,
-		tag: [TAG.WEB, TAG.GAME, TAG.SCIENCE]
+		tag: [TAG.WEB, TAG.GAME, TAG.SCIENCE],
 	},
 	{
 		name: 'FormulaZ',
 		path: 'formulaz',
 		lore: 'A collection of formulas in physics, mathematics and other subjects with a convenient search',
 		enabled: true,
-		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.MATH, TAG.PHYSIC, TAG.SCIENCE]
+		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.MATH, TAG.PHYSIC, TAG.SCIENCE],
 	},
 	{
 		name: 'history_line',
 		path: 'history_line',
 		lore: 'Just a line with branches, what being historical events',
 		enabled: false,
-		tag: [TAG.WEB, 'history', TAG.SCIENCE, TAG.FOR_ALL]
+		tag: [TAG.WEB, 'history', TAG.SCIENCE, TAG.FOR_ALL],
 	},
 	{
 		name: 'Laplas Matrix Helper',
 		path: 'laplas_matrix_helper',
 		lore: 'This utility will allow you to avoid arithmetic errors, but you do all the actions yourself!\n(saves time, exports the solution in .TeX)',
 		enabled: true, weight: -2,
-		tag: [TAG.WEB, TAG.UTIL, TAG.TEX, TAG.MATH]
+		tag: [TAG.WEB, TAG.UTIL, TAG.TEX, TAG.MATH, TAG.SCIENCE],
 	},
 	{
 		name: 'Layout Translator',
 		path: 'layout_translator',
 		lore: 'Insert mistyped russian or ukrainian text and receive the right version',
 		enabled: true,
-		tag: [TAG.WEB, TAG.UTIL, 'translator', TAG.FOR_ALL]
+		tag: [TAG.WEB, TAG.UTIL, 'translator', TAG.FOR_ALL],
 	},
 	{
 		name: 'mathaz',
 		path: 'mathaz',
 		lore: '',
 		enabled: false,
-		tag: [TAG.WEB]
+		tag: [TAG.WEB],
 	},
 	{
 		name: 'Normal Markov Algorithm',
 		path: 'normal_markov_algorithm',
 		lore: 'Now you can use comfortable normal_markov_algorithm',
 		enabled: true,
-		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING]
+		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING, TAG.SCIENCE],
 	},
 	{
 		name: 'Regexp Algorithm',
 		path: 'regexp_algorithm',
 		lore: 'Same as Normal Markov algorithm, but with RegExp!',
 		enabled: true,
-		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING, TAG.REGEX]
+		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING, TAG.REGEX, TAG.SCIENCE],
 	},
 	{
 		name: 'Sapper',
 		path: 'sapper',
 		lore: 'Almost a classic sapper Game!',
 		enabled: true,
-		tag: [TAG.WEB, TAG.GAME, TAG.FOR_ALL]
+		tag: [TAG.WEB, TAG.GAME, TAG.FOR_ALL],
 	},
 	{
 		name: 'Score Table',
 		path: 'score_table',
 		lore: 'Just some editable score tables',
 		enabled: true,
-		tag: [TAG.WEB]
+		tag: [TAG.WEB],
 	},
 	{
 		name: 'Terr\'ize',
 		path: 'seize_territory',
 		lore: 'One-device multiplayer game',
 		enabled: true,
-		tag: [TAG.WEB, TAG.GAME]
+		tag: [TAG.WEB, TAG.GAME],
 	},
 	{
 		name: 'Tattoo Templater',
 		path: 'tattoo_templater',
 		lore: 'Helps you to convert any image to simple contour like monochrome tattoo template',
 		enabled: true, weight: 3,
-		tag: [TAG.WEB, TAG.UTIL, TAG.FOR_ALL]
+		tag: [TAG.WEB, TAG.UTIL, TAG.FOR_ALL],
 	},
 	{
 		name: 'Turing Machine',
 		path: 'turing_machine',
 		lore: 'Now you can use comfortable turing_machine redactor',
 		enabled: true, weight: 5,
-		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING]
+		tag: [TAG.WEB, TAG.UTIL, TAG.PROGRAMMING, TAG.SCIENCE],
 	},
 	{
 		name: 'web_syntax',
 		path: 'web_syntax',
 		lore: 'Just an online editor with highlighted syntax for different languages',
 		enabled: false, weight: 2,
-		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.PROGRAMMING]
+		tag: [TAG.WEB, TAG.UTIL, TAG.REGEX, TAG.PROGRAMMING],
 	},
 	{
 		name: 'D&D Manager',
 		path: 'dnd_manager',
 		lore: 'This app will help you as game master or player in managing',
 		enabled: false,
-		tag: [TAG.WEB,]
+		tag: [TAG.WEB],
 	},
 ]
 
@@ -160,8 +160,8 @@ let plist = document.getElementById('project_list')
 function displayProjects(tags = []) {
 	plist.innerHTML = ''
 	for (const project of PROJECTS
-	.filter(e => e.enabled && tags.every(i => e.tag.includes(i)))
-	.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0))
+		.filter(e => e.enabled && tags.every(i => e.tag.includes(i)))
+		.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0))
 		) {
 		const tile = document.createElement('a')
 		tile.classList.add('plist_tile')
